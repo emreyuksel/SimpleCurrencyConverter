ea80b3000ac247807a742b6fc101ea2bfb75ae1f
emreyuksel <emreyuksel7@gmail.com>
2018-03-05 19:07:23 +0300

    commit-2

--- MainActivity.java
+++ MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import org.json.JSONObject;
 
 import java.io.InputStream;
